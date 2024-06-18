cd305b5198259c3321aa1a153f764ea0a9dae943
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-18 10:50:56 +0100

    sync

--- gameplay_server/gameplay_server.py
+++ gameplay_server/gameplay_server.py
@@ -1,5 +1,17 @@
+from pathlib import Path
+
+from utils.filesystem import CacheDir
+from utils.logger import Logger
 from gameplay_server.gameplay import Gameplay
 
+
 class GameplayServer:
-  def __init__(self):
+  def __init__(self, ip: str, port: int, buffer_size: int, timeout: int, cache_dir: Path = None) -> None:
+    self.ip: str = ip
+    self.port: int = port
+    self.buffer_size: int = buffer_size
+    self.timeout: int = timeout
+    name: str = str(__name__.split('.')[-1])
+    logs_dir: Path = Path(str(CacheDir())) / 'logs' if not cache_dir else cache_dir
+    self.logger: Logger = Logger(name=name, logs_dir=logs_dir)()
     self.gameplay = Gameplay()
