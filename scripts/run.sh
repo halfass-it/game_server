91b3cfca85d41ccf667e98bd8d71a35f7550f2d0
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-18 18:54:07 +0100

    sync

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,8 +3,6 @@
 CACHE="$XDG_CACHE_HOME/halfass-it/gameplay_server"
 VENV="$CACHE/.venv"
 POETRY="$VENV/bin/poetry"
-LOGS_DIR="$CACHE/logs"
-mkdir -p "$LOGS_DIR"
 if [ ! -d "$VENV" ]; then
     $POETRY env use "$VENV"
     $POETRY install
